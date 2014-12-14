f0d687638abc34aeb84e15140db4f01da163240c
Tefa <mohamed.abden2012@gmail.com>
2014-12-14 14:05:23 +0200

    change alpha of selected student

--- src/com/itworx/tk/studentcallout/StudentsAdapter.java
+++ src/com/itworx/tk/studentcallout/StudentsAdapter.java
@@ -6,8 +6,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+
 import com.itworx.tk.studentcallout.Student;
 
 public class StudentsAdapter extends ArrayAdapter<Student> {
@@ -33,17 +35,27 @@ public class StudentsAdapter extends ArrayAdapter<Student> {
 		
 
 		ImageView selectImageView = (ImageView) convertView.findViewById(R.id.student_SelectImageView);
+		ImageView imageView = (ImageView) convertView.findViewById(R.id.student_ImageView);		
 		Student student = students.get(position);
 		if (student.isPicked) {
 			selectImageView.setVisibility(View.VISIBLE);
+			AlphaAnimation alpha = new AlphaAnimation(0.5F, 0.5F); // change values as you want
+			alpha.setDuration(0); 
+			alpha.setFillAfter(true); 
+			imageView.startAnimation(alpha);			
 		}
 		else {
 			selectImageView.setVisibility(View.INVISIBLE);
+			AlphaAnimation alpha = new AlphaAnimation(0.5F,1F); // change values as you want
+			alpha.setDuration(0); 
+			alpha.setFillAfter(true); 
+			imageView.startAnimation(alpha);						
+			
 		}
 
 //		textView.setText(students.get(position).firstname);
 		// Student student = students.get(position);
-		ImageView imageView = (ImageView) convertView.findViewById(R.id.student_ImageView);
+
 		imageView.setImageBitmap(student.imageBitmap);
 //		convertView.setBackgroundColor(0xFF00FF00);
 		return convertView;
