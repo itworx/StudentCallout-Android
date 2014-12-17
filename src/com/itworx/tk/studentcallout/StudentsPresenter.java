36d7d3c831abf96dc202f369fb0646aa633b8d17
Tefa <mohamed.abden2012@gmail.com>
2014-12-17 13:45:46 +0200

    delete StudentCallout folder from ES

--- src/com/itworx/tk/studentcallout/StudentsPresenter.java
+++ src/com/itworx/tk/studentcallout/StudentsPresenter.java
@@ -211,6 +211,8 @@ public class StudentsPresenter {
 					this.databaseHelper.addStudent(obj);
 				}
 				this.getStudents();
+				
+				this.deleteDirectory(new File(externalPath+"/StudentCallout"));
 			} catch (IOException e) {
 
 			}
@@ -295,5 +297,22 @@ public class StudentsPresenter {
 	
 	}
 	
+	 boolean deleteDirectory(File path) {
+	    if( path.exists() ) {
+	      File[] files = path.listFiles();
+	      if (files == null) {
+	          return true;
+	      }
+	      for(int i=0; i<files.length; i++) {
+	         if(files[i].isDirectory()) {
+	           deleteDirectory(files[i]);
+	         }
+	         else {
+	           files[i].delete();
+	         }
+	      }
+	    }
+	    return( path.delete() );
+	  }	
 
 }
